5ddb1d0f32b56836227f6214eb0ca209a5d98d94
Tobias Grandi <granditobias@hotmail.com>
2025-10-14 15:09:30 -0300

    actualizacion hab princ

--- src/data/principalSkills.js
+++ src/data/principalSkills.js
@@ -8,7 +8,6 @@ const principalSkills = [
     {title: "Machine Learning", skills: [
         {name: "PyTorch", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg"},
         {name: "Scikit-Learn", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg"},
-        {name: "TensorFlow", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg"},
         {name: "Jupyter", img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original.svg"},
     ]}
 ]

415ccdb8bc2b08e9e2702e0e5220a71908c8b5fe
Tobias Grandi <granditobias@hotmail.com>
2025-10-14 12:58:41 -0300

    Íconos en titulo de proyecto

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -8,16 +8,23 @@ function ProjectDetail({ project }) {
 
     return (
         <div className={styles.detail}>
-            <h2>{project.title}</h2>
+
+            <div className={styles.titleContainer}>
+                <img src={project.icon} alt='Proyect Icon' className={styles.icon}></img> 
+                <h2>{project.title}</h2>
+                <img src={project.icon} alt='Proyect Icon' className={styles.icon}></img> 
+            </div>
+
             <div className={styles.detailText}>
                 {project.description.split('\n').map((line, index) => (
                     <p key={index}>{line}</p>
                 ))}
             </div>
-            <div className={styles.iconsContainer}>
+
+            <div className={styles.sourceContainer}>
                 {project.sources.map((source, index) => (
                     <a href={source.link} target="blank" rel='noopener noreferrer' key={index}> 
-                        <img src={source.img} alt={source.alt} className={styles.icon}></img>
+                        <img src={source.img} alt={source.alt} className={styles.source}></img>
                         {source.source}
                    </a>
                 ))}

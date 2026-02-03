fe9ed3347b6f477c1eed6e5c98b12cd61a9b8b20
Tobias Grandi <granditobias@hotmail.com>
2026-02-03 15:44:44 -0300

    fix

--- src/data/papers.js
+++ src/data/papers.js
@@ -7,7 +7,7 @@ import river from '../assets/proyectsIcons/river.png'
 
 const papers = [
     {
-        id: "paper-3",
+        id: "paper-1",
         title: "Predicción del nivel del Río Gualeguaychú: construcción de un conjunto de datos y modelo basado en redes neuronales LSTM",
         description: `Las inundaciones fluviales se originan cuando los ríos y arroyos exceden su capacidad de contención, desbordando sus riberas y vertiéndose en terrenos adyacentes. El impacto de estas crecidas puede incluir problemas sociales de salud, destrucción de obras de infraestructura, destrucción de viviendas, entre otros. No obstante, si se aplican medidas preventivas, las inundaciones pueden no resultar catastróficas para una ciudad. Para ello es necesario un sistema que pueda predecir las crecientes con un margen de tiempo adecuado. En este trabajo se presenta un estudio realizado sobre el río Gualeguaychú para la construcción de un modelo predictivo de estas características. En particular, se exploran fuentes de acceso abierto sobre variables relevantes para el fenómeno, se recopila y se construye un conjunto de datos apto para el uso en el entrenamiento de un modelo de aprendizaje automático. Además, como prueba de concepto, se utilizan redes neuronales tipo LSTM (Long Short Term Memory), se entrena y se evalúa su capacidad predictiva en distintos plazos de predicción. Finalmente, se propone una manera de visualizar las predicciones junto a la imprecisión propia de los pronósticos meteorológicos utilizados en el proceso predictivo. Los resultados son alentadores, aunque surgen interrogantes respecto de la calidad de las fuentes de datos y su disponibilidad en el entorno operativo.
         
@@ -18,7 +18,7 @@ const papers = [
         ]
     },
     {
-        id: "paper-1",
+        id: "paper-2",
         title: "Metrics for the Evaluation of Optimization Methods in Dynamic Problems: application to Vehicle Routing using Ant Systems",
         description: `Solving dynamic optimization problems is always a challenging task. For this purpose, heuristic and metaheuristic methods are the preferred ones, especially in the context of real-time decision making. In this sense, it is necessary to have evaluation mechanisms for such methods, in order to assist in the selection of one of them to be applied in a particular reality. In this paper we propose metrics for the evaluation of methods that solve real-time dynamic problems that do not require complete knowledge of the search space. These metrics can be used to compare methods quantitatively in various ways. Besides, the application of the proposal on variants of Ant Systems in a classical combinatorial optimization problem such as the Vehicule Routing Problem (VRP) is studied.`,
         icon: ant,
@@ -28,7 +28,7 @@ const papers = [
         ]
     },
     {
-        id: "paper-2",
+        id: "paper-3",
         title: "Agente basado en Colonia de Hormigas aplicado al problema de Enrutamiento de Vehículos en Tiempo Real",
         description: `La resolución de problemas de optimización es un área desafiante, aún más cuando estos son del tipo dinámico. En el contexto de toma de decisiones en tiempo real, los métodos metaheurísticos son ampliamente utilizados por su capacidad de brindar soluciones aceptables en tiempos cortos de respuesta. En este trabajo se propone la resolución de una variante de VRP con incertidumbre en sus componentes y actualizado en tiempo real mediante un agente inteligente basado en utilidades que utiliza ACO para la toma de decisiones.
         
